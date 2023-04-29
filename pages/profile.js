e2bbb896e8ba7a16c7a9507dd3e5c65dcc08fb8c
ysh0305 <yhung022@ucr.edu>
2023-04-29 11:59:15 -0700

    hi

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import Image from "next/image";
+import porfileIMG from "../public/ProfileIMG.svg";
+import { BsPlusCircle } from "react-icons/bs";
 const profile = () => {
-  return <div>profile</div>;
+  return (
+    <div className="flex font-teko items-center flex-col">
+      <Image src={porfileIMG} />
+      <p className=" uppercase ">Menthy Wu</p>
+      <button>EDIT</button>
+      <div>
+        <BsPlusCircle />
+        <button>Add a Friend</button>
+      </div>
+    </div>
+  );
 };
 
 export default profile;
