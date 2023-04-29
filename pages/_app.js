2ada23015db2bcb01941d45ca5ef5ffb1ed308b7
menthy-wu <wumengxuan0728@gmail.com>
2023-04-29 13:14:14 -0700

    usecontext

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,10 @@ import { Teko, Pirata_One } from "next/font/google";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import UserContext from "../components/UserContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { db, auth } from "../firebase";
 
 // eslint-disable-next-line new-cap
 const teko = Teko({
@@ -20,6 +23,23 @@ const pirata = Pirata_One({
 
 export default function App({ Component, pageProps }) {
   const [user, setUser] = useState(null);
+  const updateUserContext = async (currentUser) => {
+    const docSnap = await getDoc(doc(db, "users", currentUser.uid));
+    setUser({
+      name: docSnap.data().userName,
+      uid: currentUser.uid,
+      joinTime: docSnap.data().joinTIme,
+      friendList: docSnap.data().friendList,
+      cardList: docSnap.data().cardList,
+    });
+  };
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        updateUserContext(currentUser);
+      }
+    });
+  }, []);
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <div className={`${teko.variable} ${pirata.variable}`}>
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import AddFriends from "../components/AddFriends";
 const profile = () => {
-  return <div>profile</div>;
+  return (
+    <div>
+      <AddFriends />
+    </div>
+  );
 };
 
 export default profile;
