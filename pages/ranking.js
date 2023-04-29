2b423c5a3a036d53bf535f1e419f0ecd43a3f4c1
theo <theobfernandez@gmail.com>
2023-04-29 09:56:01 -0700

    ranking setup

--- pages/ranking.js
+++ pages/ranking.js
@@ -1,7 +1,13 @@
 import React from "react";
+import greyProfile from "../public/grey_profile.svg";
+import Image from "next/image";
 
 const ranking = () => {
-  return <div>ranking</div>;
+  return (
+    <div>
+      <Image priority src={greyProfile} alt="" />
+    </div>
+  );
 };
 
 export default ranking;
