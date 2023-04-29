453e29e90182402225c5458f9fdff3c541b14f38
menthy-wu <wumengxuan0728@gmail.com>
2023-04-29 09:58:52 -0700

    top bar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import BottomNav from "./BottomNav";
-
+import TopBar from "./TopBar";
 const Layout = ({ children }) => {
   return (
     <>
+      <TopBar />
       <main>{children}</main>
       <BottomNav />
     </>
