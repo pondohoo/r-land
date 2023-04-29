4de75e9812bffac50c5efc28d6cfdf516fcbdb0e
menthy-wu <wumengxuan0728@gmail.com>
2023-04-29 11:29:56 -0700

    usecontext

--- components/LoginButton.jsx
+++ components/LoginButton.jsx
@@ -1,42 +1,50 @@
-import React from "react";
 import { useRouter } from "next/router";
 // import UserContext from "./UserContext";
 // import { useContext } from "react";
 import { auth } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
 import {
   browserLocalPersistence,
   GoogleAuthProvider,
   setPersistence,
   signInWithPopup,
-} from "@firebase/auth";
+} from "firebase/auth";
 
-const LoginButton = () => {
-  const router = useRouter();
-  //   const { setUser } = useContext(UserContext);
+const router = useRouter();
+// const { setUser } = useContext(UserContext);
 
-  const signin = () => {
-    signInWithPopup(auth, new GoogleAuthProvider())
-      .then((result) => {
-        router.push("/collection");
-      })
-      .catch((error) => {
-        console.log(error);
-        console.log("fail sign in");
-        router.push("/");
-      });
-  };
-
-  const login = () => {
-    setPersistence(auth, browserLocalPersistence)
-      .then((res) => {
-        signin();
-      })
-      .catch((error) => {
-        router.push("/");
-      });
-  };
-
-  return <button onClick={login}>login</button>;
+const getUser = async (uid) => {
+  const docSnap = await getDoc(doc(db, "users", uid));
+  if (!docSnap.exists()) {
+  } else {
+    setUser({
+      name: docSnap.data().userName,
+      uid: uid,
+      joinTime: docSnap.data().joinTIme,
+      friendList: docSnap.data().friendList,
+      cardList: docSnap.data().cardList,
+    });
+  }
 };
 
-export default LoginButton;
+const login = () => {
+  setPersistence(auth, browserLocalPersistence)
+    .then((res) => {
+      signInWithPopup(auth, new GoogleAuthProvider())
+        .then((result) => {
+          getUser();
+          router.push("/collection");
+        })
+        .catch((error) => {
+          console.log(error);
+          console.log("fail sign in");
+          router.push("/");
+        });
+    })
+    .catch((error) => {
+      router.push("/");
+    });
+};
+
+export default login;
