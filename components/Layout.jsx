f891cc1a69d5def710ceb8ea965f880e1d31927c
ysh0305 <yhung022@ucr.edu>
2023-04-29 17:08:21 -0700

    hi

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ import BottomNav from "./BottomNav";
 import TopBar from "./TopBar";
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col justify-between w-screen h-screen">
+    <div className="flex flex-col justify-between w-screen h-screen bg-rland-black">
       <TopBar />
       <main>{children}</main>
       <BottomNav />
