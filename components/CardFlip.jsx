078345394ab92aabc92bdcf7896eea3efe2679fa
ysh0305 <yhung022@ucr.edu>
2023-04-30 00:49:06 -0700

    poggerd

--- components/CardFlip.jsx
+++ components/CardFlip.jsx
@@ -18,16 +18,24 @@ const variants = {
 
 const CardFlip = () => {
   return (
-    <motion.img
-      layout="Layout"
+    <motion.div
       key="modal"
       className=" mx-5"
-      src="/Card.svg"
       initial="initial"
       whileInView="inView"
       exit="exit"
       variants={variants}
-    />
+    >
+      <img src="/Card.svg" />
+      <div className=" -scale-x-100 mx-auto font-thin text-2xl flex text-white font-teko flex-col text-left">
+        <p className="m-0 text-left">3:00PM</p>
+        <div className="m-0 text-rland-gray flex gap-2">
+          <p className="m-0">LAT:14080923</p>
+          <p className="m-0">LONG:123932</p>
+        </div>
+        <p className="m-0 text-rland-red">5 POINTS</p>
+      </div>
+    </motion.div>
   );
 };
 
