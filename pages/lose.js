ba13deaa64e28f170294384c504d4d70b7c41369
theo <theobfernandez@gmail.com>
2023-04-30 05:29:19 -0700

    Fix padding on win and lose pages

--- pages/lose.js
+++ pages/lose.js
@@ -6,10 +6,10 @@ import { motion } from "framer-motion";
 const win = () => {
   return (
     <div className=" items-center flex-col flex h-screen w-screen relative">
-      <p className="mt-[30%] text-white font-pirata text-7xl">FAILURE</p>
+      <p className="mt-[10%] text-white font-pirata text-7xl">FAILURE</p>
       <p className="mb-[10%] text-rland-red font-teko text-2xl">-3 points</p>
       <motion.div
-        className="z-10 absolute top-[40%]"
+        className="z-10 absolute top-[15%]"
         initial={{ opacity: 1, scale: 2 }}
         animate={{ opacity: 0 }}
         transition={{ duration: 5 }}
@@ -21,7 +21,7 @@ const win = () => {
         href="/collection"
         className="mt-[15%] animate-heart-pulse"
       >
-        <Image className="mt-[450%]" src={collect} />
+        <Image className="mt-[350%]" src={collect} />
       </motion.a>
     </div>
   );
