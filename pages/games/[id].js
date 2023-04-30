9782809a2b33b28bd1470a59008bc3dc2155854b
theo <theobfernandez@gmail.com>
2023-04-29 22:52:03 -0700

    Add Game Timer

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import TapCard from "../../public/TapCard.svg";
 import { useState } from "react";
+import Timer from "../../components/Timer";
+
 
 const Game = () => {
   const [count, setCount] = useState(0);
@@ -10,6 +12,7 @@ const Game = () => {
   };
   return (
     <div className="h-full flex justify-center">
+      <Timer />
       <button onClick={handleClick} className="mt-10 w-9/12">
         <Image className="w-screen" src={TapCard} alt="" />
       </button>
