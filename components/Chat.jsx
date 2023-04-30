e1b0a5966bda53ac677670e3e1ca5b841f832578
theo <theobfernandez@gmail.com>
2023-04-30 01:20:40 -0700

    Clean up chat UI

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -39,45 +39,43 @@ const Game = () => {
   }, []);
 
   return (
-		user && (
-			<div className="mabsolute h-3/5 inset-x-0 bottom-10 flex flex-col justify-end bg-rland-darkgray/30">
-				<div className="flex h-5/6 -mb-10 m-5 flex-col flex-none gap-2">
-					<ScrollableFeed>
-						{messages.map((msg, index) => (
-							<div
-								key={index}
-								className="flex flex-none break-words items-center gap-1 text-white text-xl font-teko font-thin"
-							>
-								{user.uid == msg.uid ? (
-									<div className="bg-rland-red h-5 w-1" />
-								) : (
-									<div className="bg-rland-gray h-5 w-1" />
-								)}
-								<p className="">{msg.msg}</p>
-							</div>
-						))}
-					</ScrollableFeed>
-				</div>
+    user && (
+      <div className="mabsolute h-3/5 inset-x-0 bottom-10 flex flex-col justify-end bg-rland-darkgray/30">
+        <div className="flex h-5/6 -mb-10 m-5 flex-col flex-none gap-2">
+          <ScrollableFeed>
+            {messages.map((msg, index) => (
+              <div
+                key={index}
+                className="flex flex-none break-words items-center gap-1 text-white text-xl font-teko font-thin"
+              >
+                {user.uid == msg.uid ? (
+                  <div className="bg-rland-red h-5 w-1" />
+                ) : (
+                  <div className="bg-rland-gray h-5 w-1" />
+                )}
+                <p className="">{msg.msg}</p>
+              </div>
+            ))}
+          </ScrollableFeed>
+        </div>
 
-				
-					<form
-						className="flex mb-10 inset-x-0 items-center justify-center gap-5"
-						onSubmit={send}
-					>
-						<input
-							type="text"
-							value={message}
-							onChange={(e) => setMessage(e.target.value)}
-							className="h-full bg-rland-darkgray"
-						/>
-						<button onClick={send}>
-							<Image src={sendButton} alt="" />
-						</button>
-					</form>
-				
-			</div>
-		)
-	);
+        <form
+          className="flex mb-10 inset-x-0 items-center justify-center gap-5"
+          onSubmit={send}
+        >
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="h-full bg-rland-darkgray"
+          />
+          <button onClick={send}>
+            <Image src={sendButton} alt="" />
+          </button>
+        </form>
+      </div>
+    )
+  );
 };
 
 export default Game;
