65cc9171711ac5c6932b9bff5531bc16715c4871
theo <theobfernandez@gmail.com>
2023-04-29 23:50:45 -0700

    Revert test

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 const Layout = ({ children }) => {
   const router = useRouter();
   return (
-    <div className="fixed flex flex-col justify-between w-screen h-screen bg-rland-black">
+    <div className="flex flex-col justify-between w-screen h-screen bg-rland-black">
       <div className="h-11/12">
         <TopBar />
         <main className="h-full">{children}</main>
