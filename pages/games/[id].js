fea93192e8aacfd3eed8ae62cafdf0a46834a6b7
theo <theobfernandez@gmail.com>
2023-04-29 18:43:48 -0700

    Add chat

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -3,6 +3,7 @@ import { socket } from "../../socket";
 import UserContext from "../../components/UserContext";
 import Image from "next/image";
 import sendButton from "../../public/sendButton.svg";
+import ScrollableFeed from "react-scrollable-feed";
 
 const Game = () => {
   const [message, setMessage] = useState("");
@@ -41,36 +42,43 @@ const Game = () => {
   }, []);
 
   return (
-    <div className="bg-rland-black h-full">
-      <button onClick={tap}>TAP</button>
-      <br />
-      <div className="h-1/2 overflow-y-scroll absolute inset-x-0 bottom-10 flex flex-col justify-end bg-rland-darkgray/30">
-        <div className="w-5/6 m-7 flex flex-col flex-none gap-2 h-full">
-          {messages.map((msg, index) => (
-            <div
-              key={index}
-              className="flex truncate items-center gap-1 text-white text-xl font-teko font-thin"
-            >
-              <div className="bg-rland-red h-5 w-1" />
-              <p className="">{msg.msg}</p>
-            </div>
-          ))}
-        </div>
+		<div className="bg-rland-black h-full">
+			<button onClick={tap}>TAP</button>
+			<br />
 
-        <div className="flex mb-10 inset-x-0 justify-center gap-5">
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="w-3/4 bg-rland-darkgray"
-          />
-          <button onClick={send}>
-            <Image src={sendButton} alt="" />
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+			<div className="h-1/2 absolute inset-x-0 bottom-10 flex flex-col justify-end bg-rland-darkgray/30">
+				<div className="flex h-5/6 -mb-5 m-5 flex-col flex-none gap-2">
+        <ScrollableFeed>
+					{messages.map((msg, index) => (
+						<div
+							key={index}
+							className="flex flex-none break-words items-center gap-1 text-white text-xl font-teko font-thin"
+						>
+							{user.uid == msg.uid ? (
+								<div className="bg-rland-red h-5 w-1" />
+							) : (
+								<div className="bg-rland-gray h-5 w-1" />
+							)}
+							<p className="">{msg.msg}</p>
+						</div>
+					))}
+          </ScrollableFeed>
+				</div>
+
+				<div className="flex mb-10 inset-x-0 justify-center gap-5">
+					<input
+						type="text"
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+						className="w-3/4 bg-rland-darkgray"
+					/>
+					<button onClick={send}>
+						<Image src={sendButton} alt="" />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Game;
