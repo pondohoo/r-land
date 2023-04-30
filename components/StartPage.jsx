878ad983073ae5bb334ab79499baee275d0f9054
theo <theobfernandez@gmail.com>
2023-04-30 00:42:26 -0700

    Fix homepage

--- components/StartPage.jsx
+++ components/StartPage.jsx
@@ -11,7 +11,7 @@ import LoginButton from "./LoginButton";
 const StartPage = () => {
   return (
     <div className=" max-h-screen relative flex bg-black w-screen h-screen flex-col items-center overflow-hidden">
-      <Image className=" mt-[13%] z-10" src={usInRLand} />
+      <Image className="z-10" src={usInRLand} />
       <div className=" h-1/6 w-1/3 mt-[30%] z-10 justify-evenly flex flex-row">
         <Image src={glowCube} />
         <Image src={glowSpade} />
@@ -19,10 +19,10 @@ const StartPage = () => {
       </div>
       <Image className="absolute bottom-3" src={patternBar} />
       <div className="flex justify-center animate-heart-pulse">
-        <Image className=" top-[15%] z-0 absolute max-w-lg" src={bluryHeart} />
+        <Image className=" top-[7%] z-0 absolute max-w-lg" src={bluryHeart} />
         <motion.a
           whileTap={{ opacity: 0.2, scale: 1 }}
-          className=" duration-300   mt-[130%]"
+          className=" duration-300   mt-[110%]"
         >
           <LoginButton />
         </motion.a>
