71211209f7c72e282991e7f064f8b222621fdd45
theo <theobfernandez@gmail.com>
2023-04-29 23:45:24 -0700

    Test

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,11 +7,13 @@ import { useRouter } from "next/router";
 const Layout = ({ children }) => {
   const router = useRouter();
   return (
-    <div className="flex flex-col justify-between w-screen h-screen bg-rland-black">
-      <TopBar />
-      <main className="h-full">{children}</main>
-      {router.asPath.includes("games") ? <Chat /> : null}
-      <BottomNav />
+    <div className="fixed flex flex-col justify-between w-screen h-screen bg-rland-black">
+      <div className="h-11/12">
+        <TopBar />
+        <main className="h-full">{children}</main>
+        {router.asPath.includes("games") ? <Chat /> : null}
+        <BottomNav />
+      </div>
     </div>
   );
 };
