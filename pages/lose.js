2208b810d3b40fae70b69df94f8de856872877d1
ysh0305 <yhung022@ucr.edu>
2023-04-30 03:37:00 -0700

    add animation

--- pages/lose.js
+++ pages/lose.js
@@ -2,21 +2,27 @@ import React from "react";
 import Image from "next/image";
 import patternBar from "../public/Pattern_bar.svg";
 import collect from "../public/collect.svg";
-import CardBack from "../public/CardBack.svg";
 import { motion } from "framer-motion";
 
 const win = () => {
   return (
-    <div className="items-center flex-col flex h-screen w-screen relative">
+    <div className=" items-center flex-col flex h-screen w-screen relative">
       <p className="mt-[30%] text-white font-pirata text-7xl">FAILURE</p>
       <p className="mb-[10%] text-rland-red font-teko text-2xl">-3 points</p>
-      <Image className=" opacity-70" height={350} src={CardBack} />
+      <motion.div
+        className="z-10 absolute top-[40%]"
+        initial={{ opacity: 1, scale: 2 }}
+        animate={{ opacity: 0 }}
+        transition={{ duration: 5 }}
+      >
+        <img src="/CardBack.svg" />
+      </motion.div>
       <motion.a
         whileTap={{ opacity: 0.2 }}
         href="/collection"
-        className="mt-[15%]"
+        className="mt-[15%] animate-heart-pulse"
       >
-        <Image src={collect} />
+        <Image className="mt-[450%]" src={collect} />
       </motion.a>
 
       <Image className="absolute top-3" src={patternBar} />
--- pages/win.js
+++ pages/win.js
@@ -2,21 +2,35 @@ import React from "react";
 import Image from "next/image";
 import patternBar from "../public/Pattern_bar.svg";
 import collect from "../public/collect.svg";
-import CardBack from "../public/CardGlowBack.svg";
 import { motion } from "framer-motion";
 
 const win = () => {
   return (
-    <div className="items-center flex-col flex h-screen w-screen relative">
+    <div className=" items-center flex-col flex h-screen w-screen relative">
       <p className="mt-[30%] text-white font-pirata text-7xl">VICTORY</p>
       <p className="mb-[10%] text-rland-red font-teko text-2xl">+3 points</p>
-      <Image height={350} src={CardBack} />
+      <motion.div
+        className="z-10 absolute top-[25%]"
+        initial={{ opacity: 1, scale: 1 }}
+        animate={{ opacity: 0, scale: 0.8 }}
+        transition={{ duration: 5 }}
+      >
+        <img src="/CardGlowBack.svg" />
+      </motion.div>
+      <motion.div
+        className="z-0 top-[35%] absolute"
+        initial={{ opacity: 0, scale: 1.2 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 5 }}
+      >
+        <img src="/Card.svg" />
+      </motion.div>
       <motion.a
         whileTap={{ opacity: 0.2 }}
         href="/collection"
-        className="mt-[15%]"
+        className="animate-heart-pulse mt-[15%]"
       >
-        <Image src={collect} />
+        <Image className="mt-[450%]" src={collect} />
       </motion.a>
 
       <Image className="absolute top-3" src={patternBar} />
