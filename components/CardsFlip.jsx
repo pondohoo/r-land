eef48c9579cc1647a80c5bf6066add2dc116bff4
theo <theobfernandez@gmail.com>
2023-04-30 01:57:37 -0700

    Map CardFlip's

--- components/CardsFlip.jsx
+++ components/CardsFlip.jsx
@@ -2,27 +2,29 @@ import React from "react";
 import CardFlip from "./CardFlip";
 
 const cards = [
-	{
-		id: 1,
-	},
-	{
-		id: 2,
-	},
-	{
-		id: 3,
-	},
-	{
-		id: 4,
-	},
-	{
-		id: 5,
-	},
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+  {
+    id: 5,
+  },
 ];
 
 const CardsFlip = () => {
   return (
     <div className=" flex overflow-scroll">
-    {cards.map((card) => (<CardFlip key={card.id}/>))}
+      {cards.map((card) => (
+        <CardFlip key={card.id} />
+      ))}
     </div>
   );
 };
