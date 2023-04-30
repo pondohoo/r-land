bbf664f0cf1b0ef659c4093fd88cfa9f332de09c
Divyank Shah <divyank.shah.2016@gmail.com>
2023-04-29 23:32:06 -0700

    pog

--- socket.js
+++ socket.js
@@ -1,10 +1,9 @@
-// import { io } from "socket.io-client";
+import { io } from "socket.io-client";
 
 // const URL = "https://r-land-server-mugqodccoa-uw.a.run.app";
+const URL = "localhost:8080";
 
-// const URL = "";
-
-// export const socket = io(URL);
+export const socket = io(URL);
 // export const socket = null;
 
 // USE THIS WHEN WE HAVE AUTH WORKING PROPERLY
