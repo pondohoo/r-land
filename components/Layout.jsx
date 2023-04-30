2e7f377addbf14ca0a6410fe7dfe3ee47ccdaa7c
theo <theobfernandez@gmail.com>
2023-04-30 00:24:10 -0700

    Test

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 const Layout = ({ children }) => {
   const router = useRouter();
   return (
-    <div className="flex flex-col justify-between w-screen h-screen bg-rland-black">
+    <div className="fixed flex flex-col justify-between w-screen h-screen bg-rland-black">
       <TopBar />
       <main className="h-full">{children}</main>
       {router.asPath.includes("games") ? <Chat /> : null}
