30458d164f5b4e3d161ae857119cbdaf7dbc547e
theo <theobfernandez@gmail.com>
2023-04-30 00:04:28 -0700

    Fix layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,12 +8,10 @@ const Layout = ({ children }) => {
   const router = useRouter();
   return (
     <div className="flex flex-col justify-between w-screen h-screen bg-rland-black">
-      <div className="h-11/12">
-        <TopBar />
-        <main className="h-full">{children}</main>
-        {router.asPath.includes("games") ? <Chat /> : null}
-        <BottomNav />
-      </div>
+      <TopBar />
+      <main className="h-full">{children}</main>
+      {router.asPath.includes("games") ? <Chat /> : null}
+      <BottomNav />
     </div>
   );
 };
