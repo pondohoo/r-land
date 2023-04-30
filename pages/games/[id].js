2b1a12e692abe546808a8db9fbee1a69bb0b8bb0
theo <theobfernandez@gmail.com>
2023-04-29 17:59:56 -0700

    Start chat

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -6,7 +6,8 @@ import sendButton from "../../public/sendButton.svg";
 
 const Game = () => {
   const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState(["hi", "hi", "hi"]);
+  const [messages, setMessages] = useState([
+	]);
   const [count, setCount] = useState(0);
   const { user } = useContext(UserContext);
 
@@ -38,32 +39,36 @@ const Game = () => {
   }, []);
 
   return (
-    <div className="bg-rland-black h-full">
-      <div className="inset-x-0 absolute bottom-0 bg-rland-yellow opacity-30">
-        <button onClick={tap}>TAP</button>
-        <br />
-        {messages.map((msg, index) => (
-          <div key={index} className="flex items-center">
-            <div className="rounded-full bg-red-500 text-white w-8 h-8 flex justify-center items-center">
-              {msg.name}
-            </div>
-            <p>{msg.msg}</p>
-          </div>
-        ))}
-        <div className="flex gap-5">
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="bg-rland-darkgray"
-          />
-          <button onClick={send}>
-            <Image src={sendButton} alt="" />
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+		<div className="bg-rland-black h-full">
+			<button onClick={tap}>TAP</button>
+			<br />
+			<div className="h-1/2 overflow-y-scroll absolute inset-x-0 bottom-10 flex flex-col justify-end bg-rland-darkgray/30">
+				<div className="w-5/6 m-7 flex flex-col flex-none gap-2 h-full">
+					{messages.map((msg, index) => (
+						<div
+							key={index}
+							className="flex truncate items-center gap-1 text-white text-xl font-teko font-thin"
+						>
+							<div className="bg-rland-red h-5 w-1" />
+							<p className="">{msg.msg}</p>
+						</div>
+					))}
+				</div>
+
+				<div className="flex mb-10 inset-x-0 justify-center gap-5">
+					<input
+						type="text"
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+						className="w-3/4 bg-rland-darkgray"
+					/>
+					<button onClick={send}>
+						<Image src={sendButton} alt="" />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Game;
