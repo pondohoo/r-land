50d15493813cf1bce6b70108cd9cc27044e611c4
Divyank Shah <divyank.shah.2016@gmail.com>
2023-04-30 00:54:20 -0700

    poggers

--- socket.js
+++ socket.js
@@ -4,6 +4,7 @@ const URL = "https://r-land-server-mugqodccoa-uw.a.run.app";
 
 export const socket = io(URL, {
   autoConnect: false,
+  transports: ["websocket", "polling"],
 });
 // export const socket = null;
 
